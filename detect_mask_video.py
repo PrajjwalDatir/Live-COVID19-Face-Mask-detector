0fe85cbbe921c1c92e6b7a0a71d0472e0163e36e
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2020-11-08 13:45:26 +0530

    changes

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -1,20 +1,21 @@
 # import the necessary packages
+import os
+import time
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 from imutils.video import VideoStream
 import numpy as np
 import imutils
-import time
 import cv2
-import os
+
 
 def detect_and_predict_mask(frame, faceNet, maskNet):
 	# grab the dimensions of the frame and then construct a blob
 	# from it
 	(h, w) = frame.shape[:2]
 	blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
-		(104.0, 177.0, 123.0))
+	(104.0, 177.0, 123.0))
 
 	# pass the blob through the network and obtain the face detections
 	faceNet.setInput(blob)
